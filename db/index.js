ebfd5a71d3089d70b444f5ad58959ab24aee0d3e
rachel-meadows <rachelnewzealand@gmail.com>
2022-04-02 16:55:14 +1300

    Redirect user to first question after submitting name

--- db/index.js
+++ db/index.js
@@ -2,9 +2,23 @@ const config = require('./knexfile').development
 const connection = require('knex')(config)
 module.exports = {
   handleUserName,
+  getQuestion,
 }
 
 // Database functions to be called from routes
 function handleUserName(name, db = connection) {
   return db('users').insert({ name: name })
+}
+
+function getQuestion(question, db = connection) {
+  return db('questions')
+    .where('id', question)
+    .first()
+    .select(
+      'id',
+      'option_1 as option1',
+      'option_2 as option2',
+      'option_1_count as option1Count',
+      'option_2_count as option2Count'
+    )
 }
--- routes/routes.js
+++ routes/routes.js
@@ -12,22 +12,31 @@ router.get('/', (req, res) => {
 router.post('/', async (req, res) => {
   const data = req.body
   const userName = data.name
-
   // Add user to database and return id
   const userId = await db.handleUserName(userName)
-
+  // Start from the first question
+  const questionId = 1
   // Redirect to the id of the new user
-  res.redirect(`/${userId[0]}/question`)
+  res.redirect(`/${userId[0]}/${questionId}`)
 })
 
 // GET question page
-// site/:user/question
-router.get('/:user/question', (req, res) => {
-  res.send('hi')
+// site/:user/:question
+router.get('/:user/:question', async (req, res) => {
+  const userId = req.params.user
+  const questionId = Number(req.params.question)
+
+  // Get the question from the database
+  const question = {
+    question: await db.getQuestion(questionId),
+  }
+  console.log(question)
+
+  res.render('question', question)
 })
 
 // POST question page
-// site/question
-router.post('/:user/question', (req, res) => {
+// site/:user/question
+router.post('/:user/:question', async (req, res) => {
   res.send('hi')
 })
