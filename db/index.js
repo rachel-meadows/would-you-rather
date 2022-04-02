a6fe05901e2997bc59199c3a209a79e9de988207
rachel-meadows <rachelnewzealand@gmail.com>
2022-04-02 16:39:59 +1300

    Redirect user to route with userID query after submitting name

--- db/index.js
+++ db/index.js
@@ -1,4 +1,10 @@
-// Database functions to be called from routes
-
 const config = require('./knexfile').development
 const connection = require('knex')(config)
+module.exports = {
+  handleUserName,
+}
+
+// Database functions to be called from routes
+function handleUserName(name, db = connection) {
+  return db('users').insert({ name: name })
+}
--- routes/routes.js
+++ routes/routes.js
@@ -3,6 +3,31 @@ const router = express.Router()
 const db = require('../db/index')
 module.exports = router
 
+// GET Root route
 router.get('/', (req, res) => {
-  res.send('Active')
+  res.render('home')
+})
+
+// POST Root route - after name entered
+router.post('/', async (req, res) => {
+  const data = req.body
+  const userName = data.name
+
+  // Add user to database and return id
+  const userId = await db.handleUserName(userName)
+
+  // Redirect to the id of the new user
+  res.redirect(`/${userId[0]}/question`)
+})
+
+// GET question page
+// site/:user/question
+router.get('/:user/question', (req, res) => {
+  res.send('hi')
+})
+
+// POST question page
+// site/question
+router.post('/:user/question', (req, res) => {
+  res.send('hi')
 })
